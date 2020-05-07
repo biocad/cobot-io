7b6e1b5ebf1b9abaa94637a55abcb3f15f03e066
Maxim Koltsov <kolmax94@gmail.com>
2020-05-07 12:59:13 +0300

    Parse raw data from ab1 files

--- src/Bio/Sequence/Basecalled/Type.hs
+++ src/Bio/Sequence/Basecalled/Type.hs
@@ -1,11 +1,30 @@
 module Bio.Sequence.Basecalled.Type
   ( BasecalledSequence (..)
+  , BasecalledSequenceWithRawData (..)
   ) where
 
 import           Bio.Sequence (IsSequence (..), WeightedSequence)
 import           Data.Coerce  (coerce)
+import           Data.Int     (Int16 (..))
+import           Data.Vector  (Vector)
 
 newtype BasecalledSequence = BasecalledSequence (WeightedSequence Double Char)
+  deriving (Eq, Show)
+
+data BasecalledSequenceWithRawData
+  = BasecalledSequenceWithRawData
+      { bsSequence      :: BasecalledSequence
+        -- A sequence with quality.
+      , bsRawG          :: Vector Int16
+      , bsRawA          :: Vector Int16
+      , bsRawT          :: Vector Int16
+      , bsRawC          :: Vector Int16
+      , bsPeakLocations :: Vector Int
+        -- ^ Same length as 'bsSequence'.
+        --
+        -- For every base in 'bsSequence' corresponding element in 'bsPeakLocations' is
+        -- an index in one of the raw vectors.
+      }
   deriving (Eq, Show)
 
 instance IsSequence BasecalledSequence where
@@ -15,4 +34,3 @@ instance IsSequence BasecalledSequence where
 
   toSequence = coerce
   fromSequence = coerce
-
