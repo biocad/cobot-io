0588aad9c0d8b751ac78a2161b8ee9f9168e32f6
vks4git <vks_m@niuitmo.ru>
2020-10-29 16:44:07 +0300

    Tests replaced

--- test/FASTASpec.hs
+++ test/FASTASpec.hs
@@ -19,56 +19,6 @@ correctFasta = [ FastaItem "3HMX:A|PDBID|CHAIN|SEQUENCE" (bareSequence "IWELKKDV
                , FastaItem "Empty_ha_ha_ha" (bareSequence "")
                ]
 
-correctTest1 :: Text
-correctTest1 = T.unlines
-  [ ">test1"
-  , "ABCDEF"
-  , "GHIJKL"
-  , ""
-  , ">test2"
-  , "ABCDEF"
-  ]
-
-correctTest2 :: Text
-correctTest2 = T.unlines
-  [ ">test1"
-  , "ABCDEF"
-  , "GHIJKL"
-  , " "
-  , ">test2"
-  , "ABCDEF"
-  ]
-
-correctTest3 :: Text
-correctTest3 = T.unlines
-  [ ">test1"
-  , "ABCDEF"
-  , "GHIJKL"
-  , "\t"
-  , ">test2"
-  , "ABCDEF"
-  ]
-
-incorrectTest1 :: Text
-incorrectTest1 = T.unlines
-  [ "test1"
-  , "ABCDEF"
-  , "GHIJKL"
-  , ">test2"
-  , "ABCDEF"
-  ]
-
-incorrectTest2 :: Text
-incorrectTest2 = T.unlines
-  [ ">test1"
-  , "ABCDEF"
-  , "GHIJKL >test2"
-  , "ABCDEF"
-  ]
-
-correctAnswer :: Fasta Char
-correctAnswer = [FastaItem "test1" (bareSequence "ABCDEFGHIJKL"), FastaItem "test2" (bareSequence "ABCDEF")]
-
 fastaSpec :: Spec
 fastaSpec = describe "Fasta file parser." $ do
     parseFile "test/FASTA/correct.fasta"
@@ -80,13 +30,6 @@ parseFile path = do
         it "correctly parses fasta from file" $ do
             fasta <- fromFile path
             fasta `shouldBe` correctFasta
-    describe "various parser tests" $ do
-        it "correctly parses empty lines" $ checkParser correctTest1 (Right correctAnswer)
-        it "correctly parses empty lines with spaces" $ checkParser correctTest2 (Right correctAnswer)
-        it "correctly parses empty lines with tabs" $ checkParser correctTest3 (Right correctAnswer)
-        it "correctly fails to parse a name without >" $ checkParser incorrectTest1 (Left "endOfInput")
-        it "correctly fails to parse a new sequence at the same line" $ checkParser incorrectTest2 (Left "endOfInput")
-
 
 writeFile :: FilePath -> Spec
 writeFile path = describe "writeFile" $ do
@@ -96,5 +39,3 @@ writeFile path = describe "writeFile" $ do
         removeFile path
         fasta `shouldBe` correctFasta
 
-checkParser :: Text -> Either String (Fasta Char) -> Expectation
-checkParser source expectation = parseOnly (fastaP <* endOfInput) source `shouldBe` expectation
