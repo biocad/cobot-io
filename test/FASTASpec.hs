061e1d4310cd0ff3e1efacd5a6898d6dccf11a3f
Maxim Koltsov <kolmax94@gmail.com>
2022-10-04 10:54:34 +0300

    version 0.1.5.0 rewrite FASTA parser to Megaparsec (#67)
    Co-authored-by: Grigoriy <mikheevg@Grigoriys-MacBook-Pro.local>
    Co-authored-by: Grigoriy Mikheev <grigoriymihtt@gmail.com>
    Co-authored-by: Grigoriy Mikheev <mikheevg.@biocad.ru>

--- test/FASTASpec.hs
+++ test/FASTASpec.hs
@@ -2,37 +2,79 @@
 
 module FASTASpec where
 
-import Bio.FASTA        (fromFile, toFile)
-import Bio.FASTA.Type   (Fasta, FastaItem (..))
-import Bio.Sequence     (bareSequence)
-import Prelude          hiding (readFile, writeFile)
-import System.Directory (removeFile)
+import Bio.FASTA              (fastaP, fromFile, toFile)
+import Bio.FASTA.Parser       (parseOnly)
+import Bio.FASTA.Type         (Fasta, FastaItem (..))
+import Bio.Sequence           (bareSequence)
+import Control.Monad.IO.Class (liftIO)
+import Data.Text.IO           (readFile)
+import Prelude                hiding (readFile, writeFile)
+import System.Directory       (removeFile)
 import Test.Hspec
 
-correctFasta :: Fasta Char
-correctFasta = [ FastaItem "3HMX:A|PDBID|CHAIN|SEQUENCE" (bareSequence "IWELKKDVYVVELDWYPDAPGEMVVLTCDTPEEDGITWTLDQSSEVLGSGKTLTIQVKEFGDAGQYTCHKGGEVLSHSLL")
-               , FastaItem "7HMX:A|PDBID|CHAIN|SEQUENCE" (bareSequence "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEVLGSGKTLTIQVKEFGDAGQYTCHKGGEVLSHSLL")
-               , FastaItem "With_spaces" (bareSequence "MDFFDLDIEIKQERLPAECSLNSPLNYSLSAQLTDRMTPRTENVRRQRERMDFFDLDIEIKQERLPAECSLNSPLNYSLSAQLTDRMTPRTENVRRQRERMDFFDLDIEIKQERLPAECSLNSPLNYSLSAQLTDRMTPRTENVRRQRER")
-               , FastaItem "Empty_ha_ha_ha" (bareSequence "")
-               ]
+correctFasta1 :: Fasta Char
+correctFasta1 = [ FastaItem "3HMX:A|PDBID|CHAIN|SEQUENCE" (bareSequence "IWELKKDVYVVELDWYPDAPGEMVVLTCDTPEEDGITWTLDQSSEVLGSGKTLTIQVKEFGDAGQYTCHKGGEVLSHSLL")
+                , FastaItem "7HMX:A|PDBID|CHAIN|SEQUENCE" (bareSequence "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEVLGSGKTLTIQVKEFGDAGQYTCHKGGEVLSHSLL")
+                , FastaItem "With_spaces" (bareSequence "MDFFDLDIEIKQERLPAECSLNSPLNYSLSAQLTDRMTPRTENVRRQRERMDFFDLDIEIKQERLPAECSLNSPLNYSLSAQLTDRMTPRTENVRRQRERMDFFDLDIEIKQERLPAECSLNSPLNYSLSAQLTDRMTPRTENVRRQRER")
+                , FastaItem "Empty_ha_ha_ha" (bareSequence "")
+                ]
+
+badFasta2 :: Either String (Fasta Char)
+badFasta2 = Left "input.fasta:2:5:\n  |\n2 | ACGT....TCG\r\n  |     ^^\nunexpected \"..\"\nexpecting end of input, end of line, or letter\n"
+
+
+correctFasta3 :: Fasta Char
+correctFasta3 = [ FastaItem "N-His-E4Orf6-7-R2(115)" (bareSequence "TGATGGTGATGGTGATGcatGTGGTAAACTCGACTTTCACTTTTCTCTATCACTGATAGGGAGTGGTAAACTCGACTTTCACTTTTCTCTATCACTGATAGGGAaacagtcagcc")
+                ]
+
+badFasta4 :: Either String (Fasta Char)
+badFasta4 = Left "input.fasta:5:8:\n  |\n5 | HindIII-BFP_F                \r\n  |        ^^\nunexpected \"-B\"\nexpecting end of input, end of line, or letter\n"
+
+correctFasta5 :: Fasta Char
+correctFasta5 = [FastaItem "qCHO49 F" (bareSequence "TGGAGAGATGGCTCGAGGTTqCHORTGGTTGCTGGGAATTGAACTC")]
+
+badFasta6 :: Either String (Fasta Char)
+badFasta6 = Left "input.fasta:22:1:\n   |\n22 | sPA-LoxP-NheI_R           \r\n   | ^\nunexpected 's'\nexpecting '>' or end of input\n"
+
+badFasta7 :: Either String (Fasta Char)
+badFasta7 = Left "input.fasta:2:1:\n  |\n2 | 5\8217-CTTCAAGAGAGAGACCTGCGT-3\8217\r\n  | ^\nunexpected '5'\nexpecting '>', end of input, end of line, or sequence\n"
+
+badFasta8 :: Either String (Fasta Char)
+badFasta8 = Left "input.fasta:21:5:\n   |\n21 | CMV + enhMCK + prcTnT-2\r\n   |     ^^\nunexpected \"+ \"\nexpecting end of input, end of line, or letter\n"
 
 fastaSpec :: Spec
-fastaSpec = describe "Fasta file parser." $ do
-    parseFile "test/FASTA/correct.fasta"
-    writeFile "test/FASTA/test.fasta"
+fastaSpec = describe "Fasta files parser." $ do
+    parseFile "test/FASTA/order1.fasta" correctFasta1
+    writeFile "test/FASTA/input.fasta" correctFasta1
+    parseBadFile "test/FASTA/order2.fasta" badFasta2
+    parseFile "test/FASTA/order3.fasta" correctFasta3
+    writeFile "test/FASTA/input.fasta" correctFasta3
+    parseBadFile "test/FASTA/order4.fasta" badFasta4
+    parseFile  "test/FASTA/order5.fasta" correctFasta5
+    writeFile "test/FASTA/input.fasta" correctFasta5
+    parseBadFile "test/FASTA/order6.fasta" badFasta6
+    parseBadFile "test/FASTA/order7.fasta" badFasta7
+    parseBadFile "test/FASTA/order8.fasta" badFasta8
 
-parseFile :: FilePath -> Spec
-parseFile path = do
+parseFile :: FilePath -> Fasta Char -> Spec
+parseFile path cf = do
     describe "fromFile" $ do
         it "correctly parses fasta from file" $ do
             fasta <- fromFile path
-            fasta `shouldBe` correctFasta
+            fasta `shouldBe` cf
 
-writeFile :: FilePath -> Spec
-writeFile path = describe "writeFile" $ do
+parseBadFile :: FilePath -> Either String (Fasta Char) -> Spec
+parseBadFile path cf = do
+    describe "fromFile" $ do
+         it "correctly parses fasta from file" $ do
+            res <- liftIO (readFile path)
+            let badRes = parseOnly fastaP res
+            badRes `shouldBe` cf
+
+writeFile :: FilePath -> Fasta Char -> Spec
+writeFile path cf = describe "writeFile" $ do
     it "correctly write fasta into file" $ do
-        toFile correctFasta path
+        toFile cf path
         fasta <- fromFile path
         removeFile path
-        fasta `shouldBe` correctFasta
-
+        fasta `shouldBe` cf
--- test/FastaParserSpec.hs
+++ test/FastaParserSpec.hs
@@ -1,18 +1,19 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeApplications  #-}
 
 module FastaParserSpec where
 
-import           Bio.FASTA.Parser     (fastaP)
-import           Bio.FASTA.Type       (Fasta, FastaItem (..), ModItem (..), Modification (..))
-import           Bio.Sequence         (bareSequence)
-import           Data.Attoparsec.Text (endOfInput, parseOnly)
-import           Data.Text            (Text)
-import qualified Data.Text            as T
+import           Bio.FASTA.Parser (fastaP, parseOnly)
+import           Bio.FASTA.Type   (Fasta, FastaItem (..), ModItem (..), Modification (..))
+import           Bio.Sequence     (bareSequence)
+import           Data.Bifunctor
+import           Data.Text        (Text)
+import qualified Data.Text        as T
 import           Test.Hspec
+import           Text.Megaparsec  (eof, errorBundlePretty, parse)
 
 fastaParserSpec :: Spec
-fastaParserSpec = describe "Fasta format parser." $ do
+fastaParserSpec = describe "Fasta format parser" $ do
     emptyFasta
     onlyName
     oneSequence
@@ -25,6 +26,7 @@ fastaParserSpec = describe "Fasta format parser." $ do
     sequenceWithTabsInName
     sequenceWithTabsInSequence
     sequenceWithModifications
+    sequenceWithSpaces
     toughParserTests
 
 emptyFasta :: Spec
@@ -49,19 +51,22 @@ twoSequences :: Spec
 twoSequences = describe "twoSequences" $ do
     it "correctly parses two correct sequences" $ do
         let res = parseOnly fastaP ">3HMX:A|PDBID|CHAIN|SEQUENCE\nIWELKKDVYVVELDWYPDAPGEMVVLTCDTPEEDGITWTLDQSSE\nVLGSGKTLTIQVKEFGDAGQYTCHKGGEVLSHSLL\n>7HMX:A|PDBID|CHAIN|SEQUENCE\nEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE\nVLGSGKTLTIQVKEFGDAGQYTCHKGGEVLSHSLL"
-        res `shouldBe` Right [FastaItem @Char "3HMX:A|PDBID|CHAIN|SEQUENCE" (bareSequence "IWELKKDVYVVELDWYPDAPGEMVVLTCDTPEEDGITWTLDQSSEVLGSGKTLTIQVKEFGDAGQYTCHKGGEVLSHSLL"), FastaItem @Char "7HMX:A|PDBID|CHAIN|SEQUENCE" (bareSequence "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEVLGSGKTLTIQVKEFGDAGQYTCHKGGEVLSHSLL")]
+        res `shouldBe` Right
+          [ FastaItem @Char "3HMX:A|PDBID|CHAIN|SEQUENCE" (bareSequence "IWELKKDVYVVELDWYPDAPGEMVVLTCDTPEEDGITWTLDQSSEVLGSGKTLTIQVKEFGDAGQYTCHKGGEVLSHSLL")
+          , FastaItem @Char "7HMX:A|PDBID|CHAIN|SEQUENCE" (bareSequence "EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEVLGSGKTLTIQVKEFGDAGQYTCHKGGEVLSHSLL")
+          ]
 
 sequenceWithDigit :: Spec
 sequenceWithDigit = describe "sequenceWithDigit" $ do
     it "correctly parses incorrect sequence with digit" $ do
-        let res = parseOnly fastaP ">123\nIWELKKDVYVVELDWYPDAPGEMVVLTCDTPEE4GITWTLDQSSE"
-        res `shouldBe` Right [FastaItem @Char "123" (bareSequence "")]
+        let res = parseOnly (fastaP @Char) ">123\nIWELKKDVYVVELDWYPDAPGEMVVLTCDTPEE4GITWTLDQSSE"
+        res `shouldBe` Left "input.fasta:2:34:\n  |\n2 | IWELKKDVYVVELDWYPDAPGEMVVLTCDTPEE4GITWTLDQSSE\n  |                                  ^^\nunexpected \"4G\"\nexpecting end of input, end of line, or letter\n"
 
 sequenceWithWrongName :: Spec
 sequenceWithWrongName = describe "sequenceWithWrongName" $ do
     it "correctly parses incorrect sequence with wrong name" $ do
-        let res = parseOnly fastaP "123\nIWELKKDVYVVELDWYPDAPGEMVVLTCDTPEEGITWTLDQSSE"
-        res `shouldBe` Right ([] :: Fasta Char)
+        let res = parseOnly (fastaP @Char) "123\nIWELKKDVYVVELDWYPDAPGEMVVLTCDTPEEGITWTLDQSSE"
+        res `shouldBe` Left "input.fasta:1:1:\n  |\n1 | 123\n  | ^\nunexpected '1'\nexpecting '>' or end of input\n"
 
 sequenceWithSpacesInName :: Spec
 sequenceWithSpacesInName = describe "sequenceWithSpacesInName" $ do
@@ -71,15 +76,15 @@ sequenceWithSpacesInName = describe "sequenceWithSpacesInName" $ do
 
 sequenceWithSeveralEndOfLine :: Spec
 sequenceWithSeveralEndOfLine = describe "sequenceWithSeveralEndOfLine" $ do
-    it "correctly parses sequence with several \n after name" $ do
-        let res = parseOnly fastaP ">this is my sequence\n\n\nIWELKKDVYVVELDWYPDAPGEMVVLTCDTPEEGITWTLDQSSE"
-        res `shouldBe` Right [FastaItem @Char "this is my sequence" (bareSequence "IWELKKDVYVVELDWYPDAPGEMVVLTCDTPEEGITWTLDQSSE")]
+    it "correctly parses sequence with several \\n after name" $ do
+        let res = parseOnly (fastaP @Char) ">this is my sequence\n\n\nIWELKKDVYVVELDWYPDAPGEMVVLTCDTPEEGITWTLDQSSE"
+        res `shouldBe` Right [FastaItem "this is my sequence" (bareSequence "IWELKKDVYVVELDWYPDAPGEMVVLTCDTPEEGITWTLDQSSE")]
 
 sequenceWithSeveralEndOfLineInSequence :: Spec
 sequenceWithSeveralEndOfLineInSequence = describe "sequenceWithSeveralEndOfLineInSequence" $ do
-    it "correctly parses sequence with several \n between sequence parts" $ do
-        let res = parseOnly fastaP ">this is my sequence\nIWELKKDVYVVELDWYPDAPGEMVVLTCDTPEEGITWTLDQSSE\n\n\nYYYYYYYYYYYYYYYYYYYYYYYY"
-        res `shouldBe` Right [FastaItem @Char "this is my sequence" (bareSequence "IWELKKDVYVVELDWYPDAPGEMVVLTCDTPEEGITWTLDQSSEYYYYYYYYYYYYYYYYYYYYYYYY")]
+    it "correctly parses sequence with several \\n between sequence parts" $ do
+        let res = parseOnly (fastaP @Char) ">this is my sequence\nIWELKKDVYVVELDWYPDAPGEMVVLTCDTPEEGITWTLDQSSE\n\n\nYYYYYYYYYYYYYYYYYYYYYYYY"
+        res `shouldBe` Right [FastaItem "this is my sequence" (bareSequence "IWELKKDVYVVELDWYPDAPGEMVVLTCDTPEEGITWTLDQSSEYYYYYYYYYYYYYYYYYYYYYYYY")]
 
 sequenceWithTabsInName :: Spec
 sequenceWithTabsInName = describe "sequenceWithTabsInName" $ do
@@ -99,13 +104,22 @@ sequenceWithModifications = describe "sequenceWithModifications" $ do
         let res = parseOnly fastaP ">this is my sequence\nIWEL[mU*]KKDVYV\t\t\nYY[56FAM]YY[Trololo]YY\t\n"
         res `shouldBe` Right [FastaItem "this is my sequence" (bareSequence [Letter 'I', Letter 'W', Letter 'E', Letter 'L', Mod Mod_mU_Star, Letter 'K', Letter 'K', Letter 'D', Letter 'V', Letter 'Y', Letter 'V', Letter 'Y', Letter 'Y', Mod Mod_56FAM, Letter 'Y', Letter 'Y', Mod (Unknown "[Trololo]"), Letter 'Y', Letter 'Y'])]
 
+sequenceWithSpaces :: Spec
+sequenceWithSpaces = describe "sequenceWithSpaces" $ do
+  it "correctly parses sequence with spaces" $ do
+      let res = parseOnly fastaP ">test1\nAAAA TTTT GGGG ccA\n"
+      res `shouldBe` Right [FastaItem @Char "test1" (bareSequence "AAAATTTTGGGGccA")]
+
 toughParserTests :: Spec
 toughParserTests = describe "various parser tests" $ do
     it "correctly parses empty lines" $ checkParser correctTest1 (Right correctAnswer)
     it "correctly parses empty lines with spaces" $ checkParser correctTest2 (Right correctAnswer)
     it "correctly parses empty lines with tabs" $ checkParser correctTest3 (Right correctAnswer)
-    it "correctly fails to parse a name without >" $ checkParser incorrectTest1 (Left "endOfInput")
-    it "correctly fails to parse a new sequence at the same line" $ checkParser incorrectTest2 (Left "endOfInput")
+    it "correctly parses empty lines with trailing tabs" $ checkParser correctTest4 (Right correctAnswer4)
+    it "correctly fails to parse a name without >" $ checkParser incorrectTest1
+      (Left "input.fasta:1:1:\n  |\n1 | test1\n  | ^\nunexpected 't'\nexpecting '>' or end of input\n")
+    it "correctly fails to parse a new sequence at the same line" $ checkParser incorrectTest2
+      (Left "input.fasta:3:8:\n  |\n3 | GHIJKL >test2\n  |        ^^\nunexpected \">t\"\nexpecting end of input, end of line, or letter\n")
 
 correctTest1 :: Text
 correctTest1 = T.unlines
@@ -137,6 +151,14 @@ correctTest3 = T.unlines
   , "ABCDEF"
   ]
 
+correctTest4 :: Text
+correctTest4 = "> test4\nTTTAGGTactTGT\t\t                                                                             \t\n"
+
+correctAnswer4 :: [FastaItem Char]
+correctAnswer4 =
+  [ FastaItem "test4" (bareSequence "TTTAGGTactTGT")
+  ]
+
 incorrectTest1 :: Text
 incorrectTest1 = T.unlines
   [ "test1"
@@ -157,5 +179,7 @@ incorrectTest2 = T.unlines
 correctAnswer :: Fasta Char
 correctAnswer = [FastaItem "test1" (bareSequence "ABCDEFGHIJKL"), FastaItem "test2" (bareSequence "ABCDEF")]
 
-checkParser :: Text -> Either String (Fasta Char) -> Expectation
-checkParser source expectation = parseOnly (fastaP <* endOfInput) source `shouldBe` expectation
+checkParser :: HasCallStack => Text -> Either String (Fasta Char) -> Expectation
+checkParser source expectation =
+  first errorBundlePretty (parse (fastaP <* eof) "input.fasta" source)
+    `shouldBe` expectation
